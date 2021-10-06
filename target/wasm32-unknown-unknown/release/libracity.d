024b568a441a733c5d6bb19576ebc4f8039fe261
Jerome Humbert <djeedai@gmail.com>
2021-10-06 18:23:04 +0100

    Initial manual deploy

---
+++ target/wasm32-unknown-unknown/release/libracity.d
@@ -0,0 +1,1 @@
+G:\dev\unstable\target\wasm32-unknown-unknown\release\libracity.wasm: C:\Users\jehumb\.cargo\registry\src\github.com-1ecc6299db9ec823\wasm-bindgen-0.2.69\build.rs G:\dev\unstable\src\main.rs
